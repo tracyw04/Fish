1df2ef25b6bb7cd3238207e89bced7996fd39e2f
Tracy Wang <tracywang41@gmail.com>
2025-09-05 20:47:22 -0700

    Add Player Idle Animation & Update Player Speed

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -3,8 +3,9 @@ using System;
 
 public partial class Player : CharacterBody2D
 {
-	private int speed = 50;
+	private int speed = 85;
 	private Vector2 currentVelocity;
+	private AnimationPlayer _animationPlayer;
 	
 	public override void _PhysicsProcess(double delta)
 	{
@@ -19,5 +20,34 @@ public partial class Player : CharacterBody2D
 	private void handleInput() {
 		currentVelocity = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
 		currentVelocity *= speed;
+	}
+	
+	 public override void _Ready()
+	{
+		_animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+	}
+	
+	public override void _Process(double delta)
+	{
+		if (Input.IsActionPressed("ui_left"))
+		{
+			
+		} 
+		else if (Input.IsActionPressed("ui_right")) 
+		{
+			
+		}
+		else if (Input.IsActionPressed("ui_up")) 
+		{
+			
+		}
+		else if (Input.IsActionPressed("ui_down")) 
+		{
+			
+		}
+		else
+		{
+			_animationPlayer.Play("Idle");
+		}
 	}
 }
