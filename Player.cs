3c98c5695d5800e304a0650bbd7db17ddb678969
Tracy Wang <tracywang41@gmail.com>
2025-09-05 18:54:14 -0700

    Add Basic Player Movement(LRUP)

--- Player.cs
+++ Player.cs
@@ -3,4 +3,21 @@ using System;
 
 public partial class Player : CharacterBody2D
 {
+	private int speed = 50;
+	private Vector2 currentVelocity;
+	
+	public override void _PhysicsProcess(double delta)
+	{
+		base._PhysicsProcess(delta);
+		
+		handleInput();
+		
+		Velocity = currentVelocity;
+		MoveAndSlide();
+	}
+	
+	private void handleInput() {
+		currentVelocity = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		currentVelocity *= speed;
+	}
 }
