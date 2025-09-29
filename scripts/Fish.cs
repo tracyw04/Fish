09d69ee3c264a2ef24eb078a7274a175b6c07370
Tracy Wang <tracywang41@gmail.com>
2025-09-29 03:07:04 -0700

    shark spawns too now

--- scripts/Fish.cs
+++ scripts/Fish.cs
@@ -1,20 +1,62 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public partial class Fish : Node2D
 {
 	private AnimationPlayer _animationPlayer;
+	public string Name;
+	public int Value;
+	
+	public static Dictionary<string, FishData> FishTypes = new Dictionary<string, FishData>
+	{
+		{ "blue", new FishData("blueFish", 1, GD.Load<Texture2D>("res://assets/sprites/fish.png")) },
+		{ "shark",  new FishData("shark", 10, GD.Load<Texture2D>("res://assets/sprites/shark.png")) }
+	};
 	
 	public override void _Ready()
 	{
+		makeRandom();
 		_animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
 		_animationPlayer.Play("Caught");
 		var timer = GetNode<Timer>("Timer");
 		timer.Start();
 	}
 	
+	//will make this instance a random fishData
+	public void makeRandom() {
+		var sprite = GetNode<Sprite2D>("Sprite2D");
+		var rng = new RandomNumberGenerator();
+		rng.Randomize();
+		
+		int randomInt = rng.RandiRange(0,10);
+		
+		if (randomInt < 2) {
+			sprite.Texture = FishTypes["shark"].Texture;
+			sprite.Scale = new Vector2(0.03f, 0.03f);
+			this.Name = "shark";
+			this.Value = FishTypes["shark"].Value;
+		} else {//blueFish
+			this.Name = "blueFish";
+			this.Value = FishTypes["blueFish"].Value;
+		}
+	}
+	
 	public void OnCooldownTimerTimeout()
 	{
 		QueueFree();
+	}
+}
+public class FishData
+{
+	public string Name { get; set;}
+	public int Value { get; set;}
+	public Texture2D Texture { get; set;}
+	
+	public FishData(string name, int value, Texture2D texture)
+	{
+		Name = name;
+		Value = value;
+		Texture = texture;
 	}
 }
