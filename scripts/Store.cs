ca6af48e2b3201f9d2d3930db2c0ec74805346b0
Tracy Wang <tracywang41@gmail.com>
2025-10-01 02:01:42 -0700

    Refactor Store to use Dictionary to store Rod Data

--- scripts/Store.cs
+++ scripts/Store.cs
@@ -1,9 +1,16 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public partial class Store : Control
 {
 	private PopupMenu menu;
+	
+	public static Dictionary<int, RodData> RodTypes = new Dictionary<int, RodData>
+	{
+		{ 0, new RodData("Standard Fishing Rod", 100, GD.Load<Texture2D>("res://assets/sprites/fishingRod2.png")) },
+		{ 1,  new RodData("Magic Fishing Rod", 350, GD.Load<Texture2D>("res://assets/sprites/fishingRod3.png")) }
+	};
 	
 	public override void _Ready()
 	{
@@ -13,14 +20,16 @@ public partial class Store : Control
 	}
 	
 	public void buildMenu(){
-		Texture2D t = resized();
-		menu.AddIconItem(t, "Standard Fishing Rod");
+		for (int i = 0; i < RodTypes.Count; i++) {
+			Texture2D before = RodTypes[i].Texture;
+			Texture2D newRod = resized(before);
+			menu.AddIconItem(newRod, RodTypes[i].Name);
+		}
 	}
 	
-	public Texture2D resized() {
-		Texture2D t = GD.Load<Texture2D>("res://assets/sprites/fishingRod2.png");
+	public Texture2D resized(Texture2D t) {
 		Image img = t.GetImage();
-		img.Resize(32, 32);
+		img.Resize(16, 16);
 		return ImageTexture.CreateFromImage(img);
 	}
 	
@@ -32,5 +41,19 @@ public partial class Store : Control
 	
 	public void ShowMenu() {
 		menu.PopupCentered();
+	}
+}
+
+public class RodData
+{
+	public string Name { get; set;}
+	public int Value { get; set;}
+	public Texture2D Texture { get; set;}
+	
+	public RodData(string name, int value, Texture2D texture)
+	{
+		Name = name;
+		Value = value;
+		Texture = texture;
 	}
 }
